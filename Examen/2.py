dcd676a1b70b1324e938b9c6cfee8ea71d8b6dcc
Iuliana-Raluca <100780726+Iuliana-Raluca@users.noreply.github.com>
2024-01-29 11:32:23 +0200

    update 2

--- Examen/2.py
+++ Examen/2.py
@@ -1,7 +1,9 @@
 import numpy as np
 import scipy.stats as stats
+from matplotlib import pyplot as plt
 
-def posterior_grid_geom(grid_points=50, observed_head=5):
+
+def posterior_grid_geom(grid_points=50, observed_head=5, tails=9):
     #parametru observed_head care indica la care aruncare apare prima stema.
 
     grid = np.linspace(0, 1, grid_points)
@@ -11,6 +13,19 @@ def posterior_grid_geom(grid_points=50, observed_head=5):
     posterior /= posterior.sum()
     return grid, posterior
 
+data = np.repeat([0, 1], (10, 3))
+points = 10
+h = data.sum()
+t = len(data) - h
+
+
+grid, posterior = posterior_grid_geom(points, h, t)
+
+plt.plot(grid, posterior, 'o-')
+plt.title(f'heads = {h}, tails = {t}')
+plt.yticks([])
+plt.xlabel('θ')
+plt.show()
 grid, posterior = posterior_grid_geom()
 print("Grid:", grid)
 print("Posterior:", posterior)
@@ -19,7 +34,7 @@ print("Posterior:", posterior)
 # Vom utiliza functia argmax din NumPy pentru a gasi indicele valorilor maxime din distributia a posteriori
 
 
-# apelam functia pentru a obtine grila și distribuția a posteriori
+# apelam functia pentru a obtine grila si distributia a posteriori
 grid, posterior = posterior_grid_geom()
 
 # cautarea indicele maxim a distributiei a posteriori
